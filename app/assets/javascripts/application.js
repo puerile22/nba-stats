75ddf9b6da205f86f8cbf3695464eb8a9032cd19
Peng Gao <puerile22@gmail.com>
2015-01-20 19:59:36 -0800

    added angular module and home-page-controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require angular
+//= require angular-resource
+//= require angular-route
+//= require app.js.erb
 //= require_tree .
